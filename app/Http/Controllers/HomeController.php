580a6dda7dc519092670d5787b1d1e37ca165b61
Aivaras <stasiulis.aivaras@gmail.com>
2021-06-28 00:22:42 +0300

    Added feed view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('home');
+        $view = view('home');
+
+        if (Auth::user()) {
+            $view = view('feed');
+        }
+
+        return $view;
     }
 }
