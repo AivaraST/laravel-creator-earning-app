e46f9a88f8ac75d1341554c05fb26454a7308a3a
Aivaras <stasiulis.aivaras@gmail.com>
2021-06-28 22:27:42 +0300

    Updated header nav links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,12 +4,31 @@
                                                                 alt="Logo header"></a>
 
         <div class="header__access">
-            <a href="{{ route('login') }}" class="header__access-btn">
-                Log In
-            </a>
-            <a href="{{ route('register') }}" class="header__access-btn header__access-btn--highlighted">
-                Sign Up
-            </a>
+            @guest()
+                <a href="{{ route('login') }}" class="header__access-btn">
+                    Log In
+                </a>
+                <a href="{{ route('register') }}" class="header__access-btn header__access-btn--highlighted">
+                    Sign Up
+                </a>
+            @endguest
+            @auth()
+                <nav class="header__auth-nav">
+                    <ul class="header__auth-list">
+                        <li class="header__auth-list-item"><a href="{{ route('user.edit') }}"
+                                                              class="header__auth-link">My Profile</a></li>
+                        <li class="header__auth-list-item"><a href="{{ route('articles.index') }}"
+                                                              class="header__auth-link">Articles</a></li>
+                        <li class="header__auth-list-item"><a href="#" class="header__auth-link">Subscribers</a></li>
+                    </ul>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="header__auth-logout" onclick="event.preventDefault();
+                                                this.closest('form').submit();">Logout
+                        </button>
+                    </form>
+                </nav>
+            @endauth()
         </div>
     </div>
 </header>
