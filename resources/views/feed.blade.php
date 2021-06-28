f61858c90ff41e1c856db4a91861cf571beb58d4
Aivaras <stasiulis.aivaras@gmail.com>
2021-06-28 22:28:04 +0300

    Added feed page

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -3,7 +3,16 @@
 @section('content')
     <div class="container-fluid">
         <div class="content">
-           feed
+            @if($articles)
+                @foreach($articles as $article)
+                    <h2>{{$article->title}}</h2>
+                    {!! $article->body !!}
+                    <div>
+                        Autorius:
+                        <a href="{{route('profile', $article->user->username)}}">{{ $article->user->username }}</a>
+                    </div>
+                @endforeach
         </div>
+        @endif
     </div>
 @endsection
